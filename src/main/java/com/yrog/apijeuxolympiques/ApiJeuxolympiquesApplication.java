9cac91da2c4c524511041951c606889c05d13a38
Grégory Fulgueiras <fulgueiras.gregory@gmail.com>
2025-03-06 20:32:46 +0100

    Initial commit

---
+++ src/main/java/com/yrog/apijeuxolympiques/ApiJeuxolympiquesApplication.java
@@ -0,0 +1,13 @@
+package com.yrog.apijeuxolympiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJeuxolympiquesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJeuxolympiquesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yrog/apijeuxolympiques/ApiJeuxolympiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrog.apijeuxolympiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJeuxolympiquesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
