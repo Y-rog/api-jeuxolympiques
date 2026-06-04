c49f9fb3b002ba94324b19fbbff46fbaf34fd2eb
Grégory <fulgueiras.gregory@gmail.com>
2026-06-04 21:54:48 +0200

    fix: set EventDTO eventDateTime pattern to dd/MM/yyyy HH:mm

--- src/main/java/com/yrog/apijeuxolympiques/dto/event/EventDTO.java
+++ src/main/java/com/yrog/apijeuxolympiques/dto/event/EventDTO.java
@@ -34,6 +34,6 @@ public class EventDTO {
         private Integer eventPlacesNumber;
 
         @NotNull(message = "La date et l'heure sont obligatoires")
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        @JsonFormat(pattern = "dd/MM/yyyy HH:mm", shape = JsonFormat.Shape.STRING)
         private LocalDateTime eventDateTime;
 }
